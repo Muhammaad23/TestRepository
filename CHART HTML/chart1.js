b043b3e060d401107a679642fce2249ed5841719
Muhammaad23 <muhammadmominov2004@gmail.com>
2024-08-27 22:56:10 +0500

    first commit

---
+++ CHART HTML/chart1.js
@@ -0,0 +1,76 @@
+const ctx = document.getElementById('barchart').getContext('2d');
+const barchart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Safari', 'Chrome', 'Firefox', 'CriOs', 'Opera', 'Others'],
+        datasets: [{
+            label: 'Pageviews by Browsers',
+            data: [121, 193, 108, 49, 94, 33],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+
+
+const barCtx = document.getElementById('barChart').getContext('2d');
+    const barChart = new Chart(barCtx, {
+        type: 'bar',
+        data: {
+            labels: ['OK', 'Like', 'Facebook', 'Jobs', 'OK', 'Plus', 'Chrome', 'Tiktok', 'Twitter', 'Telegram', 'instagram', 'YouTube'],
+            datasets: [{
+                label: 'Sales ($)',
+                data: [600, 1500, 1700, 1000, 800, 500, 900, 1200, 1700, 2000, 2400, 2500],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                x: {
+                    beginAtZero: true
+                },
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
---
+++ CHART HTML/chart2.js
@@ -0,0 +1,77 @@
+const ctx2 = document.getElementById('doughnut').getContext('2d');
+const doughnut = new Chart(ctx2, {
+    type: 'doughnut',
+    data: {
+        labels: ['Safari', 'Chrome', 'Firefox', 'CriOs', 'Opera', 'Others'],
+        datasets: [{
+            label: 'Pageviews by static',
+            data: [120, 190, 100, 50, 80, 30],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+
+
+const lineCtx = document.getElementById('lineChart').getContext('2d');
+    const lineChart = new Chart(lineCtx, {
+        type: 'line',
+        data: {
+            labels: ['OK', 'Like', 'Facebook', 'Jobs', 'IT MY lance', 'Plus', 'Chrome', 'Tiktok', 'Twitter', 'Telegram', 'instagram', 'YouTube'],
+            datasets: [{
+                label: 'Growth (%)',
+                data: [600, 1000, 1700, 1300, 800, 500, 1500, 1500, 1700, 1100, 2400, 2500],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 2,
+                fill: true
+            }]
+        },
+        options: {
+            scales: {
+                x: {
+                    beginAtZero: true
+                },
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
